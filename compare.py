230e50c3075b098d2ce4d2884c308fca7827a8ed
David Owen <owen@Davids-Mac-Pro.local>
2018-02-08 16:11:46 -0500

    Added grouping based on top T closest cells (T > 1).

--- compare.py
+++ compare.py
@@ -10,19 +10,24 @@ from cells import *
 
 M = 8  # Margin to be included in cell image.
 P = 4  # Padding not to be include in template image.
-S = 0.4  # Match result score threshold.
+S = 0.5  # Match result score threshold.
+T = 3  # Number of (top) cell matches to consider when making groups.
+B = 245 # Cell with average above this considered blank (max 255).
 
-# TODO Memo this for efficiency...
 def compare(cropped, cell_a, cell_b):
     x0, y0 = cell_a[0]
     x1, y1 = cell_a[1]
     a = cropped[(y0 - M):(y1 + M), (x0 - M):(x1 + M)]
     ta = cropped[(y0 + P):(y1 - P), (x0 + P):(x1 - P)]
     
+    if np.mean(ta) > B: return 0
+    
     x0, y0 = cell_b[0]
     x1, y1 = cell_b[1]
     b = cropped[(y0 - M):(y1 + M), (x0 - M):(x1 + M)]
     tb = cropped[(y0 + P):(y1 - P), (x0 + P):(x1 - P)]
+
+    if np.mean(tb) > B: return 0
 
     m = cv2.matchTemplate(a, tb, cv2.TM_CCOEFF_NORMED)
     n = cv2.matchTemplate(b, ta, cv2.TM_CCOEFF_NORMED)
@@ -45,7 +50,6 @@ if __name__ == '__main__':
     highlighted = np.copy(cropped)
    
     for column in cells[1:-1]:
-    # column = cells[5]
         scores = [False] * len(column)
         
         for i in range(2, len(column) - 2):
@@ -53,35 +57,51 @@ if __name__ == '__main__':
                 score = compare(erased, column[i], column[j])
                 
                 if score > S:
-                    if not scores[i] or (score > scores[i][0]):
-                        scores[i] = (score, j)
+                    if not scores[i]:
+                        scores[i] = []
                     
-                    if not scores[j] or (score > scores[j][0]):
-                        scores[j] = (score, i)
-
-        # TODO Lookup connected components algorithm and
-        #      do this right...
-        groups = []
+                    if not scores[j]:
+                        scores[j] = []
+                    
+                    scores[i].append((score, j))
+                    scores[j].append((score, i))
         
         for i in range(len(scores)):
             if scores[i]:
-                j = scores[i][1]
-            
-                for g in groups:
+                scores[i].sort()
+                scores[i].reverse()
+                scores[i] = scores[i][:T]
+
+        groups = []
+        
+        def find_group(i):  # DFS for connected components.
+            m = scores[i]
+            scores[i] = False
+
+            for k in range(len(m)):
+                j = m[k][1]
+                q = False
+                
+                for g in groups[:-1]:
                     if j in g:
                         g.append(i)
-                        break
-                else:
-                    groups.append([i])
-
-                scores[i] = False
+                        q = True
                 
-                while scores[j]:
+                if q: break
+                
+                if scores[j]:
                     groups[-1].append(j)
-                    k = scores[j][1]
-                    scores[j] = False
-                    j = k
-
+                    find_group(j)
+                        
+        for i in range(len(scores)):
+            if scores[i]:
+                groups.append([i])
+                find_group(i)
+                
+                if len(groups[-1]) == 1:
+                    groups = groups[:-1]
+       
+        print(groups)
         k = 0
         
         for i in range(min(len(groups), len(COLORS))):
@@ -94,4 +114,4 @@ if __name__ == '__main__':
             k = (k + 5) % len(COLORS)
 
     cv2.addWeighted(cropped, 0.85, highlighted, 0.15, 0, dst=cropped)
-    cv2.imwrite(n + '_compare.jpg', original)
+    cv2.imwrite(n + '_compare_closest.jpg', original)
